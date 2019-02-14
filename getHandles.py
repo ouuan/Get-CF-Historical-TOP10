951c76c6eaba83b1eb11283e56bfb0d07e8d5f33
Anguei <Anguei@users.noreply.github.com>
2019-02-14 15:27:07 +0800

    format code

--- getHandles.py
+++ getHandles.py
@@ -2,27 +2,27 @@ import requests
 import json
 import time
 
-url="https://codeforces.com/ratings/page/"
+url = "https://codeforces.com/ratings/page/"
 
-handles=[]
+handles = []
 
-for page in range(1,26):
+for page in range(1, 26):
     while True:
         try:
-            s=requests.get(url+str(page)).text
+            s = requests.get(url + str(page)).text
         except:
             time.sleep(1)
         else:
             break
-    p=s.find("text-align:left;padding-left:1em;")
-    while p!=-1:
-        nxt=s.find("text-align:left;padding-left:1em;",p+1)
-        sta=s.find("/profile/",p)
-        end=s.find("\"",sta)
-        handle=s[sta+9:end]
+    p = s.find("text-align:left;padding-left:1em;")
+    while p != -1:
+        nxt = s.find("text-align:left;padding-left:1em;", p + 1)
+        sta = s.find("/profile/", p)
+        end = s.find("\"", sta)
+        handle = s[sta + 9:end]
         handles.append(handle)
         print(handle)
-        p=nxt
+        p = nxt
 
-f=open("handles.json","w")
+f = open("handles.json", "w")
 f.write(json.dumps(handles))
--- getInactiveHandles.py
+++ getInactiveHandles.py
@@ -2,33 +2,33 @@ import requests
 import json
 import time
 
-url="https://codeforces.com/ratings/all/true/page/"
+url = "https://codeforces.com/ratings/all/true/page/"
 
-with open("handles.json","r") as handlesjson:
-    handles=json.load(handlesjson)
+with open("handles.json", "r") as handlesjson:
+    handles = json.load(handlesjson)
 
-tot=0
-page=274
+tot = 0
+page = 274
 
-while tot<5000:
-    page+=1
+while tot < 5000:
+    page += 1
     while True:
         try:
-            s=requests.get(url+str(page)).text
+            s = requests.get(url + str(page)).text
         except:
             time.sleep(1)
         else:
             break
-    p=s.find("outdated")
-    while p!=-1 and tot<5000:
-        nxt=s.find("outdated",p+1)
-        sta=s.find("/profile/",p)
-        end=s.find("\"",sta)
-        handle=s[sta+9:end]
+    p = s.find("outdated")
+    while p != -1 and tot < 5000:
+        nxt = s.find("outdated", p + 1)
+        sta = s.find("/profile/", p)
+        end = s.find("\"", sta)
+        handle = s[sta + 9:end]
         handles.append(handle)
-        tot+=1
+        tot += 1
         print(handle)
-        p=nxt
+        p = nxt
 
-f=open("handles.json","w")
+f = open("handles.json", "w")
 f.write(json.dumps(handles))
